013a458fa10822cf8a240e54dbdb965bb3b2fe79
Asad Ur Rehman <114389119+asadaa007@users.noreply.github.com>
2023-10-31 01:28:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function calculateOvertime() {
+    var monthlySalary = parseFloat(
+      document.getElementById("monthlySalary").value
+    );
+    var hoursWorked = parseFloat(
+      document.getElementById("hoursWorked").value
+    );
+    var weekendHours = parseFloat(
+      document.getElementById("weekendHours").value
+    );
+
+    if (isNaN(monthlySalary) || isNaN(hoursWorked) || isNaN(weekendHours)) {
+      document.getElementById("overtimePay").innerText =
+        "Please provide complete information";
+    } else {
+      var workingHoursPerMonth = 160; // Assuming 40 hours per week and 4 weeks per month
+      var hourlyRate = monthlySalary / workingHoursPerMonth;
+      var regularHours = 160;
+      var weekdaysOvertimeRate = 1.5;
+      var weekendsOvertimeRate = 2;
+
+      var regularPay = 0;
+      var overtimePay = 0;
+
+      if (hoursWorked <= regularHours) {
+        regularPay = hoursWorked * hourlyRate * weekdaysOvertimeRate;
+      } else {
+        regularPay = regularHours * hourlyRate * weekdaysOvertimeRate;
+        var extraHours = hoursWorked - regularHours;
+        overtimePay = extraHours * hourlyRate * weekdaysOvertimeRate;
+      }
+
+      overtimePay += weekendHours * hourlyRate * weekendsOvertimeRate;
+      var totalPay = regularPay + overtimePay;
+      document.getElementById("overtimePay").innerText =
+        "Total to Pay: $" + totalPay.toFixed(2);
+    }
+  }
